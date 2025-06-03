c75c444303658ff4c353c478c2822dd6215a1108
Kevin Chandra <kacol.bot@gmail.com>
2025-06-03 11:06:48 +0700

    chore: favicon

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -6,12 +6,12 @@
     <link rel="canonical" href="{{ $page->getUrl() }}">
     <meta name="description" content="{{ $page->description }}">
     <title>@yield('site_title') - {{ $page->title }}</title>
-    <link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96"/>
-    <link rel="icon" type="image/svg+xml" href="favicon.svg"/>
-    <link rel="shortcut icon" href="favicon.ico"/>
-    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png"/>
-    <meta name="apple-mobile-web-app-title" content="Diaper Bag | BabyGo"/>
-    <link rel="manifest" href="site.webmanifest"/>
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96"/>
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
+    <link rel="shortcut icon" href="/favicon.ico"/>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+    <meta name="apple-mobile-web-app-title" content="Nuudo"/>
+    <link rel="manifest" href="/site.webmanifest"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11.2.4/swiper-bundle.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.min.css">
     <link rel="stylesheet" href="../{{ mix('/css/main.css', 'assets/build') }}">
